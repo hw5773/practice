2dc60321c0623ac16ecabf1d7e081ee9368cf0bc
Hyunwoo Lee <hw5773@gmail.com>
2023-01-23 00:20:22 +0900

    update the time series scripts

---
+++ python/time-series/time-series-keras.py
@@ -0,0 +1,88 @@
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+
+import pickle as pkl
+import keras
+from keras.models import Sequential, Model, load_model
+from keras.layers import LSTM, Dense, RepeatVector, TimeDistributed, Input, BatchNormalization, multiply, concatenate, Flatten, Activation, dot
+from keras.callbacks import EarlyStopping
+import pydot as pyd
+from keras.utils.vis_utils import plot_model, model_to_dot
+keras.utils.vis_utils.pydot = pyd
+
+def truncate(x, feature_cols=range(3), target_cols=range(3), label_col=3, train_len=100, test_len=20):
+    in_, out_, lbl = [], [], []
+    for i in range(len(x)-train_len-test_len+1):
+        in_.append(x[i:(i+train_len), feature_cols].tolist())
+        out_.append(x[(i+train_len):(i+train_len+test_len), target_cols].tolist())
+        lbl.append(x[i+train_len, label_col])
+    return np.array(in_), np.array(out_), np.array(lbl)
+
+n_ = 1000
+t = np.linspace(0, 50*np.pi, n_)
+
+x1 = sum([20*np.sin(i*t+np.pi) for i in range(5)]) + 0.01*(t**2) + np.random.normal(0, 6, n_)
+x2 = sum([15*np.sin(2*i*t+np.pi) for i in range(5)]) + 0.5*t + np.random.normal(0, 6, n_)
+
+train_ratio = 0.8
+train_len = int(train_ratio * t.shape[0])
+print ("train_len: {}".format(train_len))
+
+x_index = np.array(range(len(t)))
+x1_trend_param = np.polyfit(x_index[:train_len], x1[:train_len], 2)
+x2_trend_param = np.polyfit(x_index[:train_len], x2[:train_len], 1)
+
+print ("x1_trend_param: {}".format(x1_trend_param))
+print ("x2_trend_param: {}".format(x2_trend_param))
+
+x1_trend = (x_index ** 2) * x1_trend_param[0] + x_index * x1_trend_param[1] + x1_trend_param[2]
+x2_trend = x_index * x2_trend_param[0] + x2_trend_param[1]
+
+x1_detrend = x1 - x1_trend
+x2_detrend = x2 - x2_trend
+
+#print ("x1_detrend: {}".format(x1_detrend))
+#print ("x2_detrend: {}".format(x2_detrend))
+
+x_lbl = np.column_stack([x1_detrend, x2_detrend, x_index, [1]*train_len+[0]*(len(x_index)-train_len)])
+print ("x_lbl.shape: {}".format(x_lbl.shape))
+print ("x_lbl: {}".format(x_lbl))
+
+x_train_max = x_lbl[x_lbl[:, 3]==1, :2].max(axis=0)
+print ("x_train_max 1: {}".format(x_train_max))
+x_train_max = x_train_max.tolist() + [1]*2
+print ("x_train_max 2: {}".format(x_train_max))
+
+x_normalize = np.divide(x_lbl, x_train_max)
+print ("x_normalize: {}".format(x_normalize))
+
+"""
+plt.figure(figsize=(15, 4))
+#plt.plot(range(len(x1)), x1, label='x1')
+#plt.plot(range(len(x1_trend)), x1_trend, linestyle='--', label='x1_trend')
+#plt.plot(range(len(x2)), x2, label='x2')
+#plt.plot(range(len(x2_trend)), x2_trend, linestyle='--', label='x2_trend')
+plt.plot(range(len(x1_detrend)), x1_detrend, label='x1_detrend')
+plt.plot(range(len(x2_detrend)), x2_detrend, label='x2_detrend')
+plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.15), fancybox=True, shadow=False, ncol=2)
+plt.show()
+
+plt.figure(figsize=(15, 4))
+plt.plot(range(train_len), x_normalize[:train_len, 0], label='x1_train_normalized')
+plt.plot(range(train_len), x_normalize[:train_len, 1], label='x2_train_normalized')
+plt.plot(range(train_len, len(x_normalize)), x_normalize[train_len:, 0], label='x1_test_normalized')
+plt.plot(range(train_len, len(x_normalize)), x_normalize[train_len:, 1], label='x1_test_normalized')
+plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.15), fancybox=True, shadow=False, ncol=2)
+plt.show()
+"""
+
+X_in, X_out, lbl = truncate(x_normalize, feature_cols=range(3), target_cols=range(3), label_col=3, train_len=200, test_len=20)
+print("X_in.shape: {}, X_out.shape:{}, lbl.shape: {}".format(X_in.shape, X_out.shape, lbl.shape))
+
+X_input_train = X_in[np.where(lbl==1)]
+X_output_train = X_out[np.where(lbl==1)]
+X_input_test = X_in[np.where(lbl==0)]
+X_output_test = X_out[np.where(lbl==0)]
+print("X_input_train.shape: {}, X_output_train.shape: {}".format(X_input_train.shape, X_output_train.shape))
+print("X_input_test.shape: {}, X_output_test.shape: {}".format(X_input_test.shape, X_output_test.shape))
--- python/time-series/time-series.py
+++ python/time-series/time-series.py
@@ -1,11 +1,175 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
+from torch.utils.data import DataLoader, Dataset
 
 from tqdm import trange
+from tqdm import tqdm
 import random
+
+class windowDataset(Dataset):
+    def __init__(self, y, input_window, output_window, stride=1):
+        L = y.shape[0]
+        num_samples = (L - input_window - output_window) // stride + 1
+
+        X = np.zeros([input_window, num_samples])
+        Y = np.zeros([output_window, num_samples])
+
+        for i in np.arange(num_samples):
+            start_x = stride * i
+            end_x = start_x + input_window
+            X[:, i] = y[start_x:end_x]
+
+            start_y = stride * i + input_window
+            end_y = start_y + output_window
+            Y[:, i] = y[start_y : end_y]
+
+        X = X.reshape(X.shape[0], X.shape[1], 1).transpose((1, 0, 2))
+        Y = Y.reshape(Y.shape[0], Y.shape[1], 1).transpose((1, 0, 2))
+        self.x = X
+        self.y = Y
+
+        self.len = len(X)
+
+    def __getitem__(self, i):
+        return self.x[i], self.y[i]
+
+    def __len__(self):
+        return self.len
+
+class lstm_encoder(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers = 1):
+        super(lstm_encoder, self).__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+
+        self.lstm = nn.LSTM(input_size = input_size, hidden_size = hidden_size, num_layers = num_layers, batch_first = True)
+
+    def forward(self, x_input):
+        lstm_out, self.hidden = self.lstm(x_input)
+        return lstm_out, self.hidden
+
+class lstm_decoder(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers = 1):
+        super(lstm_decoder, self).__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+
+        self.lstm = nn.LSTM(input_size = input_size, hidden_size = hidden_size, num_layers = num_layers, batch_first = True)
+        self.linear = nn.Linear(hidden_size, input_size)
+
+    def forward(self, x_input, encoder_hidden_states):
+        lstm_out, self.hidden = self.lstm(x_input.unsqueeze(-1), encoder_hidden_states)
+        output = self.linear(lstm_out)
+
+        return output, self.hidden
+
+class lstm_encoder_decoder(nn.Module):
+    def __init__(self, input_size, hidden_size):
+        super(lstm_encoder_decoder, self).__init__()
+
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+
+        self.encoder = lstm_encoder(input_size = input_size, hidden_size = hidden_size)
+        self.decoder = lstm_decoder(input_size = input_size, hidden_size = hidden_size)
+
+    def forward(self, inputs, targets, target_len, teacher_forcing_ratio):
+        batch_size = inputs.shape[0]
+        input_size = inputs.shape[2]
+
+        outputs = torch.zeros(batch_size, target_len, input_size)
+
+        _, hidden = self.encoder(inputs)
+        decoder_input = inputs[:, -1, :]
+
+        for t in range(target_len):
+            out, hidden = self.decoder(decoder_input, hidden)
+            out = out.squeeze(1)
+
+            if random.random() < teacher_forcing_ratio:
+                decoder_input = targets[:, t, :]
+            else:
+                decoder_input = out
+            outputs[:, t, :] = out
+        return outputs
+    
+    def predect(self, inputs, target_len):
+        self.eval()
+        inputs = inputs.unsqueeze(0)
+        batch_size = inputs.shape[0]
+        input_size = inputs.shape[2]
+        outputs = torch.zeros(batch_size, target_len, input_size)
+        _, hidden = self.encoder(inputs)
+        decoder_input = inputs[:, -1, :]
+        for t in range(target_len):
+            out, hidden = self.decoder(decoder_input, hidden)
+            out = out.squeeze(1)
+            decoder_input = out
+            outputs[:, t, :] = out
+        return outputs.detach().numpy()[0,:,0]
+
+
+def MAPEval(y_pred, y_true):
+    return np.mean(np.abs((y_true - y_pred) / y_true)) * 100
 
 data = pd.read_csv("data.csv", encoding="CP949")
 plt.figure(figsize=(20, 5))
 plt.plot(range(len(data)), data["평균속도"])
 print (data.head())
+
+min_max_scaler = MinMaxScaler()
+data["평균속도"] = min_max_scaler.fit_transform(data["평균속도"].to_numpy().reshape(-1, 1))
+
+train = data[:-24*7]
+train = train["평균속도"].to_numpy()
+
+test = data[-24*7:]
+test = test["평균속도"].to_numpy()
+
+iw = 24 * 14
+ow = 24 * 7
+
+train_dataset = windowDataset(train, input_window=iw, output_window=ow, stride=1)
+train_loader  = DataLoader(train_dataset, batch_size=64)
+
+model = lstm_encoder_decoder(input_size=1, hidden_size=16).to(device)
+
+learning_rate = 0.01
+epoch = 3000
+optimizer = optim.Adam(model.parameters(), lr = learning_rate)
+criterion = nn.MSELoss()
+
+model.train()
+with tqdm(range(epoch)) as tr:
+    for i in tr:
+        total_loss = 0.0
+        for x, y in train_loader:
+            optimizer.zero_grad()
+            x = x.to(device).float()
+            y = y.to(device).float()
+            output = model(x, y, ow, 0.6).to(device)
+            loss = criterion(output, y)
+            loss.backward()
+            optimizer.step()
+            total_loss += loss.cpu().item()
+        tr.set_postfix(loss="{0:.5f}".format(total_loss/len(train_loader)))
+
+predict = model.predict(torch.tensor(train[-24*7*2:]).reshape(-1, 1).to(device).float(), target_len=ow)
+read = data["평균속도"].to_numpy()
+
+predict = min_max_scaler.inverse_transform(predict.reshape(-1, 1))
+real = min_max_scaler.inverse_transform(real.reshape(-1, 1))
+
+plt.figure(figsize=(20, 5))
+plt.plot(range(400, 744), real[400:], label="real")
+plt.plot(range(744-24*7, 744), predict[-24*7:], label="predict")
+
+plt.title("Test Set")
+plt.legend()
+plt.show()
+
+MAPEval(predict[-24*7:], real[-24*7:])
